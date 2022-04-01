f8767b7e6c90baa86e17ac6b61970a5696923daa
Kevin McCarthy <35618779+mccarthykp@users.noreply.github.com>
2022-04-01 18:28:48 -0400

    Users can create events

--- app.js
+++ app.js
@@ -1,20 +1,24 @@
 // Initialize express
 const express = require('express');
 const app = express();
+
 // require handlebars
 const exphbs = require('express-handlebars');
 const Handlebars = require('handlebars');
 const {allowInsecurePrototypeAccess} = require('@handlebars/allow-prototype-access');
+const { Sequelize } = require('sequelize')
 
 // Use "main" as our default layout
 app.engine('handlebars', exphbs.engine({ defaultLayout: 'main', handlebars: allowInsecurePrototypeAccess(Handlebars) }));
+
 // Use handlebars to render
 app.set('view engine', 'handlebars');
 
-// Tell our app to send the "hello world" message to our home page
-// app.get('/', (req, res) => {
-//   res.send('Hello World!');
-// });
+// INITIALIZE BODY-PARSER AND ADD IT TO APP
+const bodyParser = require('body-parser');
+app.use(bodyParser.urlencoded({ extended: true }));
+
+const models = require('./db/models');
 
 // Choose a port to listen on
 const port = process.env.PORT || 3000;
@@ -24,7 +28,23 @@ app.listen(port, () => {
   console.log('App listening on port 3000!');
 });
 
-// render 'home' layout for main page
+// index
 app.get('/', (req, res) => {
-  res.render('home', { msg: 'Handlebars are cool!' });
+  models.Event.findAll({ order: [['createdAt', 'DESC']] }).then(events => {
+    res.render('events-index', { events: events });
+  });
+});
+
+// new event
+app.get('/events/new', (req, res) => {
+  res.render('events-new', {});
+});
+
+// create event
+app.post('/events', (req, res) => {
+  models.Event.create(req.body).then(event => {
+    res.redirect(`/`);
+  }).catch((err) => {
+    console.log(err)
+  });
 });
