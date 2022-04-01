96a026f30fa07b9bef1f5ce95b10f3a98c656b4b
Kevin McCarthy <35618779+mccarthykp@users.noreply.github.com>
2022-04-01 16:17:03 -0400

    init

---
+++ app.js
@@ -0,0 +1,30 @@
+// Initialize express
+const express = require('express');
+const app = express();
+// require handlebars
+const exphbs = require('express-handlebars');
+const Handlebars = require('handlebars');
+const {allowInsecurePrototypeAccess} = require('@handlebars/allow-prototype-access');
+
+// Use "main" as our default layout
+app.engine('handlebars', exphbs.engine({ defaultLayout: 'main', handlebars: allowInsecurePrototypeAccess(Handlebars) }));
+// Use handlebars to render
+app.set('view engine', 'handlebars');
+
+// Tell our app to send the "hello world" message to our home page
+// app.get('/', (req, res) => {
+//   res.send('Hello World!');
+// });
+
+// Choose a port to listen on
+const port = process.env.PORT || 3000;
+
+// Tell the app what port to listen on
+app.listen(port, () => {
+  console.log('App listening on port 3000!');
+});
+
+// render 'home' layout for main page
+app.get('/', (req, res) => {
+  res.render('home', { msg: 'Handlebars are cool!' });
+});
